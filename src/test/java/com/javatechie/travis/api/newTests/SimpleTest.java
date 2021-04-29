63d08457cb208976c812928ffc412c7b4ac56ab2
kiryakor <kira_001ios@mail.ru>
2021-04-29 17:01:48 +0300

    QA-1: up

---
+++ src/test/java/com/javatechie/travis/api/newTests/SimpleTest.java
@@ -0,0 +1,31 @@
+package com.javatechie.travis.api.newTests;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class SimpleTest {
+
+    private WebDriver driver;
+
+    @Before
+    public void setupTest() {
+        driver = new ChromeDriver();
+    }
+
+    @After
+    public void teardown() {
+        if (driver != null) {
+            driver.quit();
+        }
+    }
+
+    @Test
+    public void test() {
+        // Test logic
+    }
+}
