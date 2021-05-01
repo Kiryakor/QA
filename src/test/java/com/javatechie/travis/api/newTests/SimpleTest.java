b8298003335e196fd1d8dc40b1a6a3c13ffc7515
kiryakor <kira_001ios@mail.ru>
2021-05-01 13:07:48 +0300

    QA-1: try

--- src/test/java/com/javatechie/travis/api/newTests/SimpleTest.java
+++ src/test/java/com/javatechie/travis/api/newTests/SimpleTest.java
@@ -1,40 +1,53 @@
 package com.javatechie.travis.api.newTests;
 
+import com.javatechie.travis.api.TravisCiCdApplication;
 import org.junit.*;
-import org.junit.runners.MethodSorters;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
+import org.junit.runner.RunWith;
+import org.openqa.selenium.By;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
 
+import java.io.IOException;
 
-@FixMethodOrder(MethodSorters.NAME_ASCENDING)
-public class SimpleTest {
+import static org.junit.Assert.assertTrue;
 
-    public static String URL;
-    public static WebDriver driver;
+public class SimpleTest extends TestBase {
 
     @BeforeClass
-    public static void setUpClass() {
+    public static void beforeTest() {
+        try {
+            initialize();
+        } catch (IOException e) {
+            System.out.println("Error while initializing the test");
+            e.printStackTrace();
+        }
     }
 
     @AfterClass
-    public static void tearDownClass() {
-        driver.quit();
-    }
-
-    @Before
-    public void setUp() {
-        URL = "https://mern-crud.herokuapp.com/";
-        //travis
-        System.setProperty("webdriver.chrome.driver", "/usr/bin/chromedriver");
-        driver = new ChromeDriver();
-    }
-
-    @After
-    public void tearDown() {
+    public static void afterTest() {
         driver.close();
     }
 
     @Test
-    public void testA_addSuccesss(){
+    public void testEmptyFields() {
+        navigateToRegistrationPage();
+
+        getObject("register.submit").click();
+        assertTrue(getObject("register.form").isDisplayed());
+    }
+
+    @Test
+    public void testPasswordLessThanSixChar() {
+        navigateToRegistrationPage();
+    }
+
+
+    @Test
+    public void testRegistrationSuccess() {
+        navigateToRegistrationPage();
     }
 }
--- src/test/java/com/javatechie/travis/api/oldTests/TravisCiCdApplicationTests.java
+++ src/test/java/com/javatechie/travis/api/oldTests/TravisCiCdApplicationTests.java
@@ -2,40 +2,41 @@ package com.javatechie.travis.api.oldTests;
 
 import com.javatechie.travis.api.parser.MyParser;
 import org.junit.Assert;
+import org.junit.Test;
 
 class TravisCiCdApplicationTests {
 
-    @org.junit.jupiter.api.Test
+    @Test
     void action_1(){
         String line = MyParser.action("5+5*1-2");
         Assert.assertEquals("8",line);
     }
 
-    @org.junit.jupiter.api.Test
+    @Test
     void action_2(){
         String line = MyParser.action("5+1*2");
         Assert.assertEquals("7",line);
     }
 
-    @org.junit.jupiter.api.Test
+    @Test
     void action_3(){
         String line = MyParser.action("5/4");
         Assert.assertEquals("1",line);
     }
 
-    @org.junit.jupiter.api.Test
+    @Test
     void action_4(){
         String line = MyParser.action("+5");
         Assert.assertEquals("Не вырные входные данные",line);
     }
 
-    @org.junit.jupiter.api.Test
+    @Test
     void action_5(){
         String line = MyParser.action("5^2");
         Assert.assertEquals("Не вырные входные данные",line);
     }
 
-    @org.junit.jupiter.api.Test
+    @Test
     void action_6(){
         String line = MyParser.action("-5+2");
         Assert.assertEquals("Не вырные входные данные",line);
