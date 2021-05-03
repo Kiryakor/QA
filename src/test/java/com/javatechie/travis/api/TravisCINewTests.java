1fd9123b18d28af20ccbecc03401751ff1519a49
kiryakor <kira_001ios@mail.ru>
2021-05-03 15:54:17 +0300

    UI-1: update tests

--- src/test/java/com/javatechie/travis/api/TravisCINewTests.java
+++ src/test/java/com/javatechie/travis/api/TravisCINewTests.java
@@ -2,22 +2,16 @@ package com.javatechie.travis.api;
 
 import org.assertj.swing.edt.GuiActionRunner;
 import org.assertj.swing.fixture.FrameFixture;
-import org.junit.After;
 import org.junit.Assert;
-import org.junit.Before;
 
 class TravisCINewTests {
 
-    private FrameFixture window;
-
-    @Before
-    void setUp() {
-        SimpleGUI frame = GuiActionRunner.execute(() -> new SimpleGUI()); window = new FrameFixture(frame);
-        window.show();
-    }
-
     @org.junit.jupiter.api.Test
     void simpleTest() {
+        SimpleGUI frame = GuiActionRunner.execute(() -> new SimpleGUI());
+        FrameFixture window = new FrameFixture(frame);
+        window.show();
+
         window.textBox().enterText("5+5+1-2");
         window.button("actionButton").click();
         Assert.assertEquals("9", window.label().text());
@@ -26,14 +20,13 @@ class TravisCINewTests {
 
     @org.junit.jupiter.api.Test
     void simpleTest2() {
+        SimpleGUI frame = GuiActionRunner.execute(() -> new SimpleGUI());
+        FrameFixture window = new FrameFixture(frame);
+        window.show();
+
         window.textBox().enterText("5+5+1-2");
         window.button("actionButton").click();
         Assert.assertEquals("9", window.label().text());
-        window.cleanUp();
-    }
-
-    @After
-    void tearDown() {
         window.cleanUp();
     }
 }
